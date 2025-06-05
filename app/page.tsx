790e2e24c945e1ad10155435ee58d7fc0901627c
rolanq <rolan.oyun@gmail.com>
2025-06-06 02:10:28 +0300

    dynamic page render

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Advertisement, initDatabase } from "./db/db";
 import AdCard from "./components/AdCard";
 // import WebAppInit from "./components/WebAppInit";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function Home() {
   try {
     await initDatabase();
