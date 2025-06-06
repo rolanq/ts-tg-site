9915e573108a451d953b051b50766729953ef5c0
rolanq <rolan.oyun@gmail.com>
2025-06-06 07:38:47 +0300

    added colors const

---
+++ app/constants/colors.ts
@@ -0,0 +1,19 @@
+export const COLORS = {
+  primary: "#b89a6a", // насыщенный бежево-коричневый
+  secondary: "#cbb089", // светлый бежевый
+  accent: "#a07a4b", // тёплый коричневый
+  background: "#f5eedc", // очень светлый бежевый
+  light: "#f3ecd7", // светлый кремовый
+  extraLight: "#f9f6ee", // почти белый
+  dark: "#6b4c36", // тёмно-коричневый
+};
+
+export const COLORS_CSS_VARS = `
+  --color-primary: #b89a6a;
+  --color-secondary: #cbb089;
+  --color-accent: #a07a4b;
+  --color-background: #f5eedc;
+  --color-light: #f3ecd7;
+  --color-extra-light: #f9f6ee;
+  --color-dark: #6b4c36;
+`;
