ffd0e00e492cad836b011e58977c54daaa6b2095
rolanq <rolan.oyun@gmail.com>
2025-06-16 00:02:06 +0300

    changed badge logic

--- app/features/Badges/Badges.tsx
+++ app/features/Badges/Badges.tsx
@@ -11,7 +11,7 @@ interface BadgesProps {
 
 export default function Badges({ ad, size = "medium" }: BadgesProps) {
   const isNew = ad.createdAt
-    ? new Date(ad.createdAt) > new Date(Date.now() - 1000 * 60 * 60 * 24)
+    ? new Date(ad.createdAt).getTime() > Date.now() - 24 * 60 * 60 * 1000
     : false;
   const isOnHold = "isOnHold" in ad && ad.isOnHold;
   const isSold = !ad.isActive;
@@ -19,9 +19,7 @@ export default function Badges({ ad, size = "medium" }: BadgesProps) {
   return (
     <CustomFlex direction="row" gap="5px" align="center">
       {isNew && <CustomBadge text="Новое" variant="success" size={size} />}
-      {isOnHold && (
-        <CustomBadge text="Задаток" variant="danger" size={size} />
-      )}
+      {isOnHold && <CustomBadge text="Задаток" variant="danger" size={size} />}
       {isSold && <CustomBadge text="Продано" variant="info" size={size} />}
     </CustomFlex>
   );
